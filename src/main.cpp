4d1c4ba4432eb3119d03b25253f65531c4ed1672
FRABONI BASILE <basile.fraboni@univ-lyon1.fr>
2019-09-17 10:50:36 +0200

    not yet.

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 
 #include "legendre.hpp"
 
-typedef Rosetta::GaussLegendreQuadrature<vec2, 3> Legendre;
+typedef Rosetta::GaussLegendreQuadrature<vec2, 2> Legendre;
 
 typedef kdtree::Point<2> Point2;
 typedef kdtree::KDTree<2> KDTree2;
@@ -109,7 +109,7 @@ int main( int argc, char * argv[] )
     // image voronoisation
     // cf:  Approximating Functions on a Mesh with Restricted Voronoï Diagrams
     //      Nivoliers V, Lévy B
-    const int max_iter = 300;
+    const int max_iter = 200;
     for(int iter = 0; iter < max_iter; ++iter)
     {
         // copy sites for nearest neighbour search
@@ -149,9 +149,8 @@ int main( int argc, char * argv[] )
             vsites[i] = cinekine::voronoi::Vertex(sites[i].x, sites[i].y);
         cinekine::voronoi::Graph graph = cinekine::voronoi::build(std::move(vsites), w, h);
 
-        
-        Legendre legendre;
         // foreach cell of the voronoi diagram compute the gradient of the objective function over edges
+        Legendre legendre;
         std::vector<vec2> gradients(size, vec2(0,0));
         #pragma omp for
         for(int i = 0; i < size; ++i)
@@ -171,136 +170,42 @@ int main( int argc, char * argv[] )
                 if( neighbor_id < 0 ) continue; // no neighbor
                 const auto& neighbor = graph.sites()[neighbor_id];
 
-                // line integral using quadrature : order 2
-                {
+                // integral of F(x) over the edge segment using quadrature
+                {   
+                    // get segment info
                     float dx = std::fabs(edge.p1.x - edge.p0.x), dy = std::fabs(edge.p1.y - edge.p0.y);
-                    vec2 n = normalize(vec2(dy, -dx));
-                    vec2 ps = sites[site_id];
-                    vec2 pn = sites[neighbor_id];
+                    vec2 n = normalize(vec2(-dy, dx));
+                    const vec2& pl = sites[site_id];
+                    const vec2& pk = sites[neighbor_id];
 
-                    vec2 p(edge.p0.x, edge.p0.y);
-                    if( p.x < 0 || p.x >= w || p.y < 0 || p.y >= h ) continue;
-                    
-                    vec2 q(edge.p1.x, edge.p1.y);
-                    if( q.x < 0 || q.x >= w || q.y < 0 || q.y >= h ) continue;
+                    vec2 p0(edge.p0.x, edge.p0.y);
+                    // if( p0.x < 0 || p0.x >= w || p0.y < 0 || p0.y >= h ) continue;
+                    vec2 p1(edge.p1.x, edge.p1.y);
+                    // if( p1.x < 0 || p1.x >= w || p1.y < 0 || p1.y >= h ) continue;
 
-                    auto project = [&dy, &dx](const vec2& p0, const vec2& p1, const vec2& p) -> vec2
-                    {
-                        float m = dy / dy;
-                        float b = p0.y - (m * p0.x);
+                    // if normal is ill-oriented return backward
+                    if( dot(n, (pk - p0)) < 0 ) n = -n;
 
-                        float x = (m * p.y + p.x - m * b) / (m * m + 1);
-                        float y = (m * m * p.y + m * p.x + b) / (m * m + 1);
-
-                        return vec2(x, y);
-                    };
-                    
-                    vec2 proj = project(vec2(edge.p0.x, edge.p0.y), vec2(edge.p1.x, edge.p1.y), ps);
-                    vec2 n2 = ps - proj;
-                    if( dot(n, n2) > 0 ) 
-                    {
-                        n = -n;
-                    }
-
-                    auto objective = [ps, pn, n, site_id, neighbor_id, &image, &colors]( vec2 p ) -> vec2 
+                    // lambda function for edge integration 
+                    auto objective = [pl, pk, n, site_id, neighbor_id, &image, &colors]( vec2 p ) -> vec2 
                     { 
                         // image gradient term
                         Color& color_pixel = image(p.x, p.y);  
                         
                         Color& color_current = colors[site_id];  
-                        float g_current = (color_pixel - color_current).length2();
+                        float gl = (color_pixel - color_current).length2();
 
                         Color& color_neighbor = colors[neighbor_id]; 
-                        float g_neighbor = (color_pixel - color_neighbor).length2();
+                        float gk = (color_pixel - color_neighbor).length2();
 
-                        float g = g_current - g_neighbor;
+                        float g = gl - gk;
 
-                        // speed vector term
-                        float dot_prod = dot(n, (pn - p));
-                        vec2 vs = dot_prod > 0 ? (ps - p) * g / dot_prod : vec2(0,0);
-
-                        return vs;
+                        // speed vector term times gradient term
+                        return g * (pl - p) / dot(n, (pl - pk));
                     };
 
-                    gradients[cell.site] = gradients[cell.site] + legendre.integrate<>(p, q, objective);
-                    
-                    // {
-                    //     // image gradient term
-                    //     Color& color_pixel = image(p.x, p.y);  
-                        
-                    //     Color& color_current = colors[site_id];  
-                    //     float g_current = (color_pixel - color_current).length2();
-
-                    //     Color& color_neighbor = colors[neighbor_id]; 
-                    //     float g_neighbor = (color_pixel - color_neighbor).length2();
-
-                    //     float g = g_current - g_neighbor;
-
-                    //     // speed vector term
-                    //     float dot_prod = dot(n, (pn - p));
-                    //     vec2 vs = dot_prod > 0 ? (ps - p) * g / dot_prod : vec2(0,0);
-
-                    //     // integrate value
-                    //     gradients[cell.site] = gradients[cell.site] + legendre;
-                    // }
-
-                    // {
-                    //     // image gradient term
-                    //     Color& color_pixel = image(q.x, q.y);  
-                        
-                    //     Color& color_current = colors[site_id];  
-                    //     float g_current = (color_pixel - color_current).length2();
-
-                    //     Color& color_neighbor = colors[neighbor_id]; 
-                    //     float g_neighbor = (color_pixel - color_neighbor).length2();
-
-                    //     float g = g_current - g_neighbor;
-
-                    //     // speed vector term
-                    //     float dot_prod = dot(n, (pn - q));
-                    //     vec2 vs = dot_prod > 0 ? (ps - q) * g / dot_prod : vec2(0,0);
-
-                    //     // integrate value
-                    //     gradients[cell.site] = gradients[cell.site] + vs;
-                    // }
+                    gradients[cell.site] = gradients[cell.site] + legendre.integrate<>(p0, p1, objective);
                 }
-                
-                // integrate gradients over edge
-                // float x0 = edge.p0.x; 
-                // float y0 = edge.p0.y; 
-                // float x1 = edge.p1.x; 
-                // float y1 = edge.p1.y; 
-                // Traverse tr(x0, y0, x1, y1);
-                // for( int nb = tr.n; nb > 0; --nb, ++tr )
-                // {
-                //     int x = tr.x, y = tr.y;
-                //     if( x < 0 || x >= w || y < 0 || y >= h ) continue; // out of bound pixel
-
-                //     // image gradient term
-                //     Color& color_pixel = image(x, y);  
-                    
-                //     Color& color_current = colors[site_id];  
-                //     float g_current = (color_pixel - color_current).length2();
-
-                //     Color& color_neighbor = colors[neighbor_id]; 
-                //     float g_neighbor = (color_pixel - color_neighbor).length2();
-
-                //     float g = g_current - g_neighbor;
-
-                //     // speed vector term
-                //     vec2 n = normalize(vec2(tr.dy, -tr.dx));
-                //     vec2 p(tr.x, tr.y);
-                //     vec2 ps = sites[site_id];
-                //     vec2 pn = sites[neighbor_id];
-
-                //     assert( pn.x == sites[neighbor_id].x );
-                //     assert( pn.y == sites[neighbor_id].y );
-                //     float dot_prod = dot(n, (pn - p));
-                //     vec2 vs = dot_prod > 0 ? (ps - p) / dot_prod : vec2(0,0);
-
-                //     // integrate value
-                //     gradients[cell.site] = gradients[cell.site] + vs * g;
-                // }
             }
             // normalize gradient
             gradients[cell.site] = normalize(gradients[cell.site]);
@@ -317,9 +222,9 @@ int main( int argc, char * argv[] )
             sites[i] = sites[i] + delta * gradients[i];
 
         {
-            std::stringstream ss;
-            ss << iter << "-" << argv[3];
-            write_image(reconstruct(image, sites), ss.str().c_str());
+            // std::stringstream ss;
+            // ss << iter << "-" << argv[3];
+            // write_image(reconstruct(image, sites), ss.str().c_str());
         }
 
         printf("iteration %d\n", iter);
